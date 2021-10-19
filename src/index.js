cc32c5d2b8a75b5d5bd759ccffcb8d3cd19e5b87
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2021-10-19 15:41:17 +0530

    SASS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import './Components/sass/main.scss';
+
 import App from './Components/App';
 
 ReactDom.render(<App />, document.querySelector('#root'));
