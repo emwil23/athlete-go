9cde3c5bbfc78025bb799e84348963884b974f4c
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2021-10-24 01:32:41 +0530

    Added signup fun

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class LoginPage extends React.Component {
-  state = { count: 2 };
+  state = { count: 1, current: 'login' };
 
   Login = () => {
     return <div>Login</div>;
@@ -16,36 +16,160 @@ class LoginPage extends React.Component {
           </div>
         </div>
       );
-    else if (counter === 2) return <div className='item-2'>
-      <b>Why Us?</b>
-      <br></br>
-      <br></br>
-       Our Site provides a very effective and effecient Diet plan 
-      <br></br>
-      for Athletes and people who have interest at staying fit.
-    </div>;
-    else if (counter === 3) return <div className='item-3'>
-    <b>How We Work!</b>
-    <br></br>
-    <br></br>
-    We Provide a diet chart to our clients which is made by our expert dietitians.
-    </div>;
-    else if(counter === 4 ) return <div className='item-4'><b>Who we are?</b>
-    <br></br>
-    <br></br>
-    We are team of expert dietitian our ditetians has helped many athelets in achieivng their goals
-    and helped them in maintaining their BMI by giving them best diet plan according to their food preferences and body needs.
-    </div>
-    else  if(counter === 5 ) return <div className='item-5'><b>Testimony</b>
-    </div>
+    else if (counter === 2)
+      return (
+        <div className='item-2'>
+          <div className='item-2--title'>Why Us?</div>
+          <p className='item-2--p'>
+            Our Site provides a very effective and effecient Diet plan for
+            Athletes and people who have interest at staying fit.
+          </p>
+        </div>
+      );
+    else if (counter === 3)
+      return (
+        <div className='item-3'>
+          <div className='item-3--title'>How We Work!</div>
+          <p className='item-3--p'>
+            We Provide a diet chart to our clients which is made by our expert
+            dietitians.
+          </p>
+        </div>
+      );
+    else if (counter === 4)
+      return (
+        <div className='item-4'>
+          <div className='item-4--title'>Who we are?</div>
+          <p className='item-4--p'>
+            We are team of expert dietitian our ditetians has helped many
+            athelets in achieivng their goals and helped them in maintaining
+            their BMI by giving them best diet plan according to their food
+            preferences and body needs.
+          </p>
+        </div>
+      );
+    else if (counter === 5)
+      return (
+        <div className='item-5'>
+          <b>Testimony</b>
+        </div>
+      );
+  };
 
+  renderForm = (current) => {
+    console.log(current);
+    if (current === 'login') {
+      return (
+        <div className='container__login-formlogin'>
+          <form className='form'>
+            <div className='form__group'>
+              <input
+                type='email'
+                className='form__input'
+                placeholder='Email'
+                id='email'
+                autoComplete='false'
+                required={true}
+              />
+              <label htmlfor='email' className='form__label'>
+                Email
+              </label>
+            </div>
+            <div className='form__group'>
+              <input
+                type='password'
+                className='form__input'
+                placeholder='**********'
+                id='email'
+                autoComplete='false'
+                required={true}
+              />
+              <label htmlfor='password' className='form__label'>
+                Password
+              </label>
+            </div>
+            <div className='form__group'>
+              <button className='form__login-btn'>&rarr;</button>
+            </div>
+          </form>
+        </div>
+      );
+    } else if (current === 'signup') {
+      return (
+        <div className='container__login-formsignup'>
+          <form className='form'>
+            <div className='form__group'>
+              <input
+                type='name'
+                className='form__input'
+                placeholder='Name'
+                id='name'
+                autoComplete='false'
+                required={true}
+              />
+              <label htmlfor='email' className='form__label'>
+                Name
+              </label>
+            </div>
+            <div className='form__group'>
+              <input
+                type='email'
+                className='form__input'
+                placeholder='Email'
+                id='emailReq'
+                autoComplete='false'
+                required={true}
+              />
+              <label htmlfor='email' className='form__label'>
+                Email
+              </label>
+            </div>
+            <div className='form__group'>
+              <input
+                type='email'
+                className='form__input'
+                placeholder='Confirm Email'
+                id='emailConf'
+                autoComplete='false'
+                required={true}
+              />
+              <label htmlfor='email' className='form__label'>
+                Confirm Email
+              </label>
+            </div>
+            <div className='form__group'>
+              <input
+                type='password'
+                className='form__input'
+                placeholder='**********'
+                id='email'
+                autoComplete='false'
+                required={true}
+              />
+              <label htmlfor='password' className='form__label'>
+                Password
+              </label>
+            </div>
+
+            <div className='form__group'>
+              <button className='form__login-btn'>&rarr;</button>
+            </div>
+          </form>
+        </div>
+      );
+    }
+  };
+
+  loginIcon = (current) => {
+    if (current === 'login') return { display: 'block' };
+    else return { display: 'none' };
   };
 
   ChangeCover = () => {
     var current = this.state.count;
     setTimeout(() => {
       current += 1;
-      if (current === 5) {
+      if (current === 6) {
         return () => clearTimeout(current);
       } else {
         this.setState({ count: current });
@@ -59,52 +183,35 @@ class LoginPage extends React.Component {
         <div className='container__login'>
           <div className='container__login-btn'>
             <button
-              className='container__login-btn--login -active'
+              onClick={() => {
+                this.setState({ current: 'login' });
+              }}
+              className={`container__login-btn--login ${
+                this.state.current === 'login' ? '-active' : ''
+              }`}
               style={{ fontSize: '1.2rem' }}
             >
               Login
             </button>
             <button
-              className='container__login-btn--signup'
+              className={`container__login-btn--signup ${
+                this.state.current === 'signup' ? '-active' : ''
+              } `}
               style={{ fontSize: '1.2rem' }}
+              onClick={() => {
+                this.setState({ current: 'signup' });
+              }}
             >
               Signup
             </button>
           </div>
-          <div className='container__login-form'>
-            <form className='form'>
-              <div className='form__group'>
-                <input
-                  type='email'
-                  className='form__input'
-                  placeholder='Email'
-                  id='email'
-                  autoComplete='false'
-                  required={true}
-                />
-                <label htmlfor='email' className='form__label'>
-                  Email
-                </label>
-              </div>
-              <div className='form__group'>
-                <input
-                  type='password'
-                  className='form__input'
-                  placeholder='**********'
-                  id='email'
-                  autoComplete='false'
-                  required={true}
-                />
-                <label htmlfor='password' className='form__label'>
-                  Password
-                </label>
-              </div>
-              <div className='form__group'>
-                <button className='form__login-btn'>&rarr;</button>
-              </div>
-            </form>
-          </div>
-          <div className='container__login-icon' />
+          {/* Renders current form */}
+          {this.renderForm(this.state.current)}
+
+          <div
+            className='container__login-icon'
+            style={this.loginIcon(this.state.current)}
+          />
         </div>
         <div className='container__cover'>
           <div className='container__cover__items'>
@@ -148,7 +255,6 @@ class LoginPage extends React.Component {
               style={{ cursor: 'pointer' }}
             ></div>
           </div>
-          
         </div>
       </div>
     );
